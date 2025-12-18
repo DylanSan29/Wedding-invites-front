1564350efb88987ba33b10135a786b8af797265d
DylanSan29 <dylansanchez2902@gmail.com>
2025-12-17 18:55:15 -0600

    Invites and refactor css and paths

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-
+import InviteList from "./pages/InviteList/InviteList";
+import PersonalInvitation from "./pages/PersonalInvitation/PersonalInvitation";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-
 import ForgotPassword from "./pages/ForgotPassword";
-import Layout from "./Layout";
-
 import PublicRoute from "./components/PublicRoute";
 
 import "./i18n";
@@ -16,14 +14,52 @@ function App() {
   return (
     <Router>
       <Routes>
-        {/* Default Layout for Regular Users */}
-        <Route path="/" element={<Layout />}>
-          {/* Public Routes */}
-          <Route path="login" element={<PublicRoute><Login /></PublicRoute>} />
-          <Route path="home" element={<PublicRoute><Home /></PublicRoute>} />
-          <Route path="register" element={<PublicRoute><Register /></PublicRoute>} />
-          <Route path="forgot-password" element={<PublicRoute><ForgotPassword /></PublicRoute>} />
-        </Route>
+        <Route path="/" element={<Home />} />
+
+        <Route
+          path="/login"
+          element={
+            <PublicRoute>
+              <Login />
+            </PublicRoute>
+          }
+        />
+
+        <Route
+          path="/inviteList"
+          element={
+            <PublicRoute>
+              <InviteList />
+            </PublicRoute>
+          }
+        />
+
+        <Route
+          path="/personalInvitation"
+          element={
+            <PublicRoute>
+              <PersonalInvitation />
+            </PublicRoute>
+          }
+        />
+
+        <Route
+          path="/register"
+          element={
+            <PublicRoute>
+              <Register />
+            </PublicRoute>
+          }
+        />
+
+        <Route
+          path="/forgot-password"
+          element={
+            <PublicRoute>
+              <ForgotPassword />
+            </PublicRoute>
+          }
+        />
       </Routes>
     </Router>
   );
---
+++ src/pages/PersonalInvitation/PersonalInvitation.jsx
@@ -0,0 +1,9 @@
+import PersonalInvitation from "../../components/PersonalInvitation/PersonalInvitation.jsx"
+
+export default function Home() {
+  return (
+    <main>
+      <PersonalInvitation/>
+    </main>
+  )
+}
